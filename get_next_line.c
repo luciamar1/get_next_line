728ce1c7067aff86045e42ca47565cea016c95b5
Lucia Jimena Martin Rodriguez <lucia-ma@c1r13s1.42madrid.com>
2022-08-15 19:07:21 +0200

    juju

--- get_next_line.c
+++ get_next_line.c
@@ -1,26 +1,26 @@
 #include "get_next_line.h"
 
-char    *get_next_line(int  fd)
+char	*get_next_line(int fd)
 {
-    char    *s;
-    static  st;
-    int     e;
+	char		*s;
+	static char	*st;
+	int			e;
 
-    e = 0;
-    s = ft_calloc(BUFFER_SIZE, 1);
-    while(*s != '\n')
-    {
-        if(s[i] != '\n')
-        {
-            while(s[i])
-                st == s[i++];    
-        }
-        read(fd, s, ft_strlen(s));
-        while(*s && *s != '\n')
-            s ++;
-        if (!s)
-            return()
-    }
-    if (*s == '\n')
-        *s = 0;
+	e = 0;
+	s = ft_calloc(BUFFER_SIZE, 1);
+	st = NULL;
+
+	while (*s != '\n')
+	{
+		if (s[i] != '\n')
+		{
+			while (s[i])
+				st[e] == s[i++];
+			s[i] = 0;
+		}
+		read(fd, s, ft_strlen(s));
+		ft_lstadd_back(ls, substr(s, 0, strlen(s)));
+		free(s);
+	}
+	s = ft_calloc(ft_lstiter(ls, size_t(ft_strlen)(char *)));
 }
