b35129e1ab8e8c10654e82503e967afaa369055c
Lucia Jimena Martin Rodriguez <lucia-ma@c1r8s4.42madrid.com>
2022-08-16 17:43:23 +0200

    k2

--- get_next_line.c
+++ get_next_line.c
@@ -5,22 +5,27 @@ char	*get_next_line(int fd)
 	char		*s;
 	static char	*st;
 	int			e;
+	t_list		ls;
+	int			i;
 
+	i = 0;
+	ls = NULL;
 	e = 0;
 	s = ft_calloc(BUFFER_SIZE, 1);
 	st = NULL;
-
 	while (*s != '\n')
 	{
 		if (s[i] != '\n')
 		{
-			while (s[i])
-				st[e] == s[i++];
-			s[i] = 0;
+			while (s++)
+				st[e++] == s;
+			*s = 0;
 		}
 		read(fd, s, ft_strlen(s));
-		ft_lstadd_back(ls, substr(s, 0, strlen(s)));
+		ft_lstadd_back(ls, substr(s, 0, ft_strlen(s)));
 		free(s);
+		s++;
 	}
 	s = ft_calloc(ft_lstiter(ls, size_t(ft_strlen)(char *)));
+	ft_poner()
 }
